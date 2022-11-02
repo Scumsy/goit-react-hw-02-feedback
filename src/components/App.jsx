e06599bbe2418472a41ef1bc9845f5be2b582ea5
Scumsy <dmitry.koschavka@gmail.com>
2022-11-02 19:40:45 +0200

    general view

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         // alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       <h1>Please leave feedback</h1>
+//     </div>
+//   );
+// };
+
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  // countTotalFeedback()
+  // countPositiveFeedbackPercentage()
+
+  onBtnClick = evt => {
+    console.log(`${evt.target} button was clicked!`, evt);
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          // display: 'flex',
+          justifyContent: 'center',
+          // alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1>Please leave feedback</h1>
+        <button onClick={this.onBtnClick}>Good</button>
+        <button onClick={this.onBtnClick}>Neutral</button>
+        <button onClick={this.onBtnClick}>Bad</button>
+        <h2>Statistics</h2>
+        <p>
+          Good:
+          <span>{this.state.good}</span>
+        </p>
+        <p>
+          Neutral:
+          <span>{this.state.neutral}</span>
+        </p>
+        <p>
+          Bad:
+          <span>{this.state.bad}</span>
+        </p>
+      </div>
+    );
+  }
+}
